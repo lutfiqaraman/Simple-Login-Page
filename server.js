af0dfc77600cdab31de75a8349aecddcb3c9d098
lutfiqaraman <lutfiqaraman@gmail.com>
2020-02-03 10:44:02 +0200

    add variables to the app post action of login

--- server.js
+++ server.js
@@ -18,16 +18,20 @@ app.use('api/*',req=>{
 
 app.post('/api/login',  (req, res) => {
 
-    if(req.body && req.body.email && req.body.password){
+    const email = req.body.email;
+    const password = req.body.password;
+
+    if(req.body && email && password){
         if(req.body.email == '123@123.123'){
 
             if(req.body.password == '123123') {
-                var user ={
-                    name:"Alex Jones"
-                    , email:req.body.email
-                    , password:req.body.password
-                    , profilePic:"http://lorempixel.com/500/500/people/"
+                var user = {
+                    name: "Alex Jones", 
+                    email: req.body.email, 
+                    password: req.body.password, 
+                    profilePic: "http://lorempixel.com/500/500/people/"
                 };
+
                 res.send(200, user);
             }
             else
