e020d959a286e4dbf952724dc786ec90993aceb2
lutfiqaraman <lutfiqaraman@gmail.com>
2020-02-22 13:10:24 +0200

    Do configuration

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const staticFiles = require("serve-static");
 
+console.log(__dirname);
+require("dotenv").config({ path: "./config/.env" });
+
+const port = process.env.PORT;
+
 //Crypto is a built-in function in nodejs
 const crypto = require("crypto");
 
@@ -23,42 +28,43 @@ app.post("/api/login", (req, res) => {
   //Define email and password variable
   const email = req.body.email;
   const password = req.body.password;
+  const databasepassword = process.env.DB_PASSWORD;
 
-  //Error Handling Array 
+  //Hasing and Salting Password
+  const salt = crypto.randomBytes(16).toString("hex");
+
+  const hashinguserpassword = crypto
+    .pbkdf2Sync(password, salt, 1000, 64, "sha512")
+    .toString("hex");
+  
+    const hashingdatabasepassword = crypto
+    .pbkdf2Sync(databasepassword, salt, 1000, 64, "sha512")
+    .toString("hex");
+
+  //Error Handling Array
   const errMsg = [
     "hey lady, you sent me the wrong password.",
     "hey man, you sent me the wrong email.",
     "yo! you miss`n some stuff!"
   ];
 
-  //Hasing and Salting Password
-  const salt = crypto.randomBytes(16).toString('hex'); 
-  const hashingpassword = crypto.pbkdf2Sync(password, salt, 1000, 64, `sha512`).toString(`hex`); 
-
-  //"123123"
   if (req.body && email && password) {
     if (email == "123@123.123") {
-      if (hashingpassword) {
+      if (hashinguserpassword == hashingdatabasepassword) {
         var user = {
           ...req.body,
-          password: hashingpassword,
+          password: hashinguserpassword,
           name: "Alex Jones",
           profilePic: "http://lorempixel.com/500/500/people/"
         };
 
         res.status(200).send(user);
-      } else
-        res
-          .status(400)
-          .send(errMsg[0])
-    } else
-      res
-        .status(400)
-        .send(errMsg[1])
+      } else res.status(400).send(errMsg[0]);
+    } else res.status(400).send(errMsg[1]);
   } else res.status(422).send(errMsg[2]);
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000);
-console.log("running on http://localhost:3000");
+app.listen(port);
+console.log("running on http://localhost:" + port);
